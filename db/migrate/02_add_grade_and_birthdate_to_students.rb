0b4d5261c8b77bcd5d0d8991074002978ecd1f82
Robert Tate <btate712@gmail.com>
2019-04-14 19:58:47 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddColumnsToStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column do |t|
       t.integer :grade
